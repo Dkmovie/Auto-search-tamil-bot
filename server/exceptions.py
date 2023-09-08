587cf109127f7c2ba3939fa3ea5bf26de7145263
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2023-09-08 17:44:27 +0530

    Create exceptions.py

---
+++ server/exceptions.py
@@ -0,0 +1,14 @@
+    # Credit @R_KOHLI.
+    # Please Don't remove credit.
+    # Born to make history @R_KOHLI !
+
+    # Thank you RAVI KOHLI for helping us in this Journey
+    # 🥰  Thank you for giving me credit @LazyDeveloperr  🥰
+    # thanks Adarsh Goel for Server Routing
+    # for any error please contact me -> telegram@LazyDeveloperr or insta @LazyDeveloperr 
+
+class InvalidHash(Exception):
+    message = "Invalid hash"
+
+class FIleNotFound(Exception):
+    message = "File not found"
